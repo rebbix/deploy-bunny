dabe8ebbf35e6789f48811f772ea71e2b985b8a3
Taras Kunch <taras.kunch@gmail.com>
2013-03-15 19:37:10 +0200

    Add notifications adn default options

--- lib/deploy_bunny.rb
+++ lib/deploy_bunny.rb
@@ -1,3 +1,5 @@
+require File.expand_path('deploy_bunny/default_options', File.dirname(__FILE__))
 require File.expand_path('deploy_bunny/advanced_options', File.dirname(__FILE__))
 require File.expand_path('deploy_bunny/locks', File.dirname(__FILE__))
 require File.expand_path('deploy_bunny/nginx', File.dirname(__FILE__))
+require File.expand_path('deploy_bunny/notifications', File.dirname(__FILE__))
---
+++ lib/deploy_bunny/default_options.rb
@@ -0,0 +1,19 @@
+Capistrano::Configuration.instance(:must_exist).load do
+  # basic options
+  set(:user)     { "deploy" }
+  set(:runner)   { user }
+  set(:use_sudo) { false }
+
+  set(:branch)    { run_locally("git rev-parse --symbolic-full-name --abbrev-ref HEAD").strip }
+  set(:deploy_to) { "/var/www/#{application}" }
+
+  set :revision,  real_revision # not lazy! Causes deadlock (recursion)
+
+  set(:deploy_via)    { :remote_cache }
+  set(:keep_releases) { 25 }
+  set(:ssh_options)   { {:forward_agent => true} }
+
+  # advanced options
+  set(:servers_yaml_path) { 'config/servers.yml' }
+  set(:options_yaml_path) { 'config/options.yml' }
+end
