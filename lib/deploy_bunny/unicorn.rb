360384a047ecd12f98edf7d2c85aa1cd994e1434
Taras Kunch <taras.kunch@gmail.com>
2013-03-28 00:09:24 +0200

    Add more descriptions

--- lib/deploy_bunny/unicorn.rb
+++ lib/deploy_bunny/unicorn.rb
@@ -40,24 +40,40 @@ Capistrano::Configuration.instance(:must_exist).load do
 
   namespace :unicorn do
     desc <<-DESC
+      Start unicorn web server.
+      Start in demonized mode from :unicorn_run_from_dir with config from :unicorn_config_file_path. \
+      It is running in :environment mode and pid is located under :unicorn_pid_file_path
+
+      Default values:
+
+        :unicorn_run_from_dir      :current_release
+        :unicorn_pid_file_path     ":unicorn_run_from_dir/tmp/pids/unicorn.pid"
+        :unicorn_config_file_path  ":unicorn_run_from_dir/config/unicorn.rb"
+
+        :unicorn_roles  [:app]
+        :unicorn_bin    'unicorn'
     DESC
     task :start, :roles => unicorn_roles do
       app_env "#{bundle_exec} #{unicorn_bin} -c #{unicorn_config_file_path} -E #{fetch(:environment, 'production')} -D"
     end
 
     desc <<-DESC
+      Gracefully stop unicorn web server.
     DESC
     task :stop, :roles => unicorn_roles do
       run kill_unicorn('QUIT')
     end
 
     desc <<-DESC
+      Forcefully stop web server with SIG TERM.
     DESC
     task :terminate, :roles => unicorn_roles do
       run kill_unicorn('TERM')
     end
 
     desc <<-DESC
+      Restart unicorn with zero downtime.
+      You should also take care about killing old master process.
     DESC
     task :soft_restart, :roles => unicorn_roles do
       transaction do
@@ -72,6 +88,7 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc <<-DESC
+      Restart unicorn by stopping old master process and starting new one.
     DESC
     task :hard_restart, :roles => unicorn_roles do
       stop
@@ -80,18 +97,21 @@ Capistrano::Configuration.instance(:must_exist).load do
     end
 
     desc <<-DESC
+      Increment unicorn workers count.
     DESC
     task :add_worker, :roles => unicorn_roles do
       run kill_unicorn('TTIN')
     end
 
     desc <<-DESC
+      Decrement unicorn workers count.
     DESC
     task :remove_worker, :roles => unicorn_roles do
       run kill_unicorn('TTOU')
     end
 
     desc <<-DESC
+      Get information if unicorn is running.
     DESC
     task :status, :roles => unicorn_roles do
       run(with_running_unicorn do
